315ad7824bff804b4c8b95213adcbd0d6c4a5bd4
ashfaqkhan <ashukhanmohammed9@gmail.com>
2023-07-15 11:36:42 +0000

    React-CP1_congratulationsCard

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="cardContainer">
+    <div>
+      <h1>Congratulations!</h1>
+    </div>
+    <div className="personCard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Ashfaq khan MD</h1>
+      <p>
+        Sree chaitanya college of computer education and technology, karimnagar.
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
